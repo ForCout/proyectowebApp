d6e7c56299d7531d5d811756e4eedae5602747c9
forcout <monchoforcout@gmail.com>
2021-01-18 20:50:31 +0100

    todo ok

--- proyectowebApp/urls.py
+++ proyectowebApp/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("contacto", views.contacto, name="Contacto"),
     path("blog", views.blog, name="Blog"),
     path("proyectos", views.proyectos, name="Proyectos"),
-    path("quiensoy", views.quiensoy, name="Quien_"),
+    path("quiensoy", views.quiensoy, name="Quien_soy"),
 
 ]
